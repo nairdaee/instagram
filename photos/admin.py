b6471dc839c10b99ccc29a6128301b76d97b601d
Adrian Etenyi <etadriano2@gmail.com>
2020-05-29 18:38:30 +0300

    register the models to the admin

--- photos/admin.py
+++ photos/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Image, Profile, Followers, Comments
 
-# Register your models here.
+admin.site.register(Image)
+admin.site.register(Profile)
+admin.site.register(Followers)
+admin.site.register(Comments)
