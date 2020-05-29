55f3254eb4b4d08e70f87e4532069cd3e5006ac4
Adrian Etenyi <etadriano2@gmail.com>
2020-05-29 17:48:14 +0300

    add sign-up,log-in and log-out views as well as authentication and user creation forms

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
+from django.contrib.auth import login,logout #function for loging in/out the user
 
-# Create your views here.
+
+def signup_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save() #saving user
+            login(request, user) 
+            
+            return redirect("photos:home")
+    else:
+
+        form = UserCreationForm()
+    return render(request,'accounts/signup.html',{'form': form})
+
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data = request.POST)
+        if form.is_valid():
+            user = form.get_user() # retrieves the user that is logging in
+            login(request, user) #loges user in if form is valid
+            if 'next' in request.POST:
+                return redirect(request.POST.get('next')) #get the value that accompanied the request and redirects to that value
+            else:                                #next is the name if the input type btw
+              return redirect("photos:home")
+    else:
+
+        form = AuthenticationForm()
+    return render(request,'accounts/login.html',{"form":form})
+
+def logout_view(request):
+    if request.method == 'POST':
+       logout(request) #will log current user out,no need to specify
+       return redirect("photos:home") #articles is the app name;list is the url name
---
+++ photos/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+from django.conf.urls.static import static
+from django.conf import settings
+from . import views
+
+app_name = 'photos'
+
+urlpatterns=[
+    url('^$', views.home, name = 'home'),
