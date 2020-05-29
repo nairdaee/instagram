fde7aafc178447a7cf61967e8051879e839eb966
Adrian Etenyi <etadriano2@gmail.com>
2020-05-29 18:03:23 +0300

    add likes and profile models

--- photos/models.py
+++ photos/models.py
@@ -63,3 +63,34 @@ class Followers(models.Model):
     
     def save_followers(self):
         self.save()
+
+class PhotoLikes(models.Model):
+    postid = models.IntegerField()
+    liker = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.liker
+    
+    def save_likes(self):
+        self.save()
+
+
+class Profile(models.Model):
+    pic=models.ImageField(upload_to='profile/',blank=True)
+    bio=models.CharField(max_length=30)
+    userId=models.IntegerField()
+
+
+    def __str__(self):
+        return self.bio
+
+    class Meta:
+        ordering=['pic']
+
+    def save_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        profile=Profile.objects.all().delete()
+        return profile
+    
